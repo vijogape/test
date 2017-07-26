cadafb69c63914f87bb76926056810bddceeb5bc
vijogape <vijoga08@gmail.com>
2017-07-26 09:31:16 -0400

    victor

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,10 +1,42 @@
-#include <iostrea>
+#include <iostream>
 
-using namespace std
+using namespace std;
 
 int main()
 {
 	string name;
 	float num1, num2;
+	double tot;
+	int opt;
+	
+	cout<<"Escribe tu nombre aqui"<<endl;
+	cin>>name;
+	cout<<"Pon tu primer numero"<<endl;
+	cin>>num1;
+	cout<<"Pon tu segundo numero"<<endl;
+	cin>>num2;
+	cout<<"elige el numero de la opracion que quieres realizar"<<endl<<"1.suma"<<endl<<"2.resta"<<endl<<"3.multiplicacion"<<endl<<"4.division"<<endl;
+	cin>>opt;
+	system("cls");
+	
+	switch(opt)
+	{
+		case 1:
+		tot=num1+num2;
+		cout<<tot;
+		break;	
+		case 2:
+		tot=num1-num2;
+		cout<<tot;
+		break;	
+		case 3:
+		tot=num1*num2;
+		cout<<tot;
+		break;	
+		case 4:
+		tot=num1/num2;
+		cout<<tot;
+		break;	
+	}
 	
 }
